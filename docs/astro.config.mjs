ab58396295c0d131d7c36b5bd08e161e1d348bca
Bennett Moore <bennett@bemoore.life>
2026-02-18 00:03:06 -0500

    feat: add CustomCard component for importing MDI icons

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,30 +1,42 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-
 import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  // This project is deployed with GitHub Pages on multiple remotes,
-  // so these fields are configured from the deployment workflow.
-  // These defaults are provided for development purposes only.
-  site: process.env.ASTRO_SITE || "https://bizrush-capstone.github.io",
-  base: process.env.ASTRO_BASE || "",
-  integrations: [starlight({
-          title: 'BizRush',
-          social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/bizrush-capstone' }],
-          sidebar: [
-              {
-                  label: 'Project',
-                  items: [
-                      { label: 'Home', slug: 'index' },
-                      { label: 'Project Overview', slug: 'project-overview' },
-                      { label: 'Month 1', slug: 'month-1' },
-                      { label: 'Month 2', slug: 'month-2' },
-                      { label: 'Month 3', slug: 'month-3' },
-                  ],
-              },
-          ],
-      }), icon()],
+    // This project is deployed with GitHub Pages on multiple remotes,
+    // so these fields are configured from the deployment workflow.
+    // These defaults are provided for development purposes only.
+    site: process.env.ASTRO_SITE || 'https://bizrush-capstone.github.io',
+    base: process.env.ASTRO_BASE || '',
+    integrations: [
+        starlight({
+            title: 'BizRush',
+            social: [
+                {
+                    icon: 'github',
+                    label: 'GitHub',
+                    href: 'https://github.com/bizrush-capstone',
+                },
+            ],
+            sidebar: [
+                {
+                    label: 'Project',
+                    items: [
+                        { label: 'Home', slug: 'index' },
+                        { label: 'Project Overview', slug: 'project-overview' },
+                        { label: 'Month 1', slug: 'month-1' },
+                        { label: 'Month 2', slug: 'month-2' },
+                        { label: 'Month 3', slug: 'month-3' },
+                    ],
+                },
+            ],
+        }),
+        icon({
+            include: {
+                mdi: ['*'],
+            },
+        }),
+    ],
 });
